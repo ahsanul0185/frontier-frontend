68c85b109e763ac74b8668fb61e9cbe8ad771a0c
Ahsanul Haque <ahsanul0185@gmail.com>
2026-05-27 13:24:37 +0200

    removed server action

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,13 +1,11 @@
-export const dynamic = 'force-dynamic';
+
 
 import FeaturedArticle from "@/components/home/FeaturedArticle";
 import articlesJson from '@/articles.json'
 import ArticleGrid from "@/components/home/ArticleGrid";
-import { getAllArticles } from "@/services/article.service";
 
 export default async function Home() {
 
-  const articles = await getAllArticles();
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-white font-sans dark:bg-black">
