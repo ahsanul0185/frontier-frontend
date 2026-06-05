76665ff516f53923882bafec241e233aa7a624e9
Ahsanul Haque <ahsanul0185@gmail.com>
2026-06-05 12:10:03 +0600

    added pagination

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,19 +1,29 @@
-
-
 import FeaturedArticle from "@/components/home/FeaturedArticle";
-import articlesJson from '@/articles.json'
 import ArticleGrid from "@/components/home/ArticleGrid";
 import { getAllArticles } from "@/services/article.service";
 
-export default async function Home() {
+export default async function Home(
+  props: {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+  }
+) {
+  const searchParams = await props.searchParams;
+  
+  const rawPage = searchParams.page;
+  let page = 1;
+  if (typeof rawPage === 'string') {
+    const parsed = parseInt(rawPage, 10);
+    if (!isNaN(parsed) && parsed > 0) {
+      page = parsed;
+    }
+  }
 
-  const articles = await getAllArticles();
-
+  const initialArticlesResponse = await getAllArticles({ page, pageSize: 6, published: true });
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-white font-sans dark:bg-black">
       <FeaturedArticle />
-       <ArticleGrid articles={articles.items} />
+       <ArticleGrid data={initialArticlesResponse} />
     </div>
   );
 }
--- src/components/home/ArticleGrid.tsx
+++ src/components/home/ArticleGrid.tsx
@@ -1,22 +1,36 @@
-// src/components/home/ArticleGrid.tsx
-import { Article, IArticle } from '@/types/article.types'
+import { IArticlePaginatedResponse } from '@/types/article.types'
 import ArticleCard from './ArticleCard'
 import AppButton from '../ui/AppButton'
+import Link from 'next/link'
 
+export default function ArticleGrid({ data }: { data: IArticlePaginatedResponse }) {
+  const hasNext = (data.page * data.pageSize) < data.total;
+  const hasPrevious = data.page > 1;
 
-
-export default function ArticleGrid({ articles }: { articles: IArticle[] }) {
   return (
     <section className="container py-16">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-14">
-        {articles.map((article) => (
+        {data.items.map((article) => (
           <ArticleCard key={article.id} article={article} />
         ))}
       </div>
-      <div className='flex items-center mt-12 '>
-        <AppButton className='mx-auto w-fit'>
-          Show More
-        </AppButton>
+      
+      <div className='flex items-center justify-center gap-4 mt-12'>
+        {hasPrevious && (
+          <Link href={`/?page=${data.page - 1}`}>
+             <AppButton className='w-fit'>
+                Previous
+             </AppButton>
+          </Link>
+        )}
+        
+        {hasNext && (
+          <Link href={`/?page=${data.page + 1}`}>
+            <AppButton className='w-fit'>
+               Next
+            </AppButton>
+          </Link>
+        )}
       </div>
     </section>
   )
